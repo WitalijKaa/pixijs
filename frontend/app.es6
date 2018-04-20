c8e217cf4c7d973d4dbe2ddeddec715af2a38511
WitalijKaa2nd <kislica@mindkitchen.ru>
2018-04-20 16:45:17 +0300

    babel и настройки среды

--- frontend/app.es6
+++ frontend/app.es6
@@ -6,10 +6,25 @@ function theApp() {
     document.body.appendChild(appEl);
 }
 
+function findPixiSize() {
+    let w = window.innerWidth;
+    let h = window.innerHeight;
+    let size = w > h ? h : w;
+    return Math.floor(size * 0.8);
+}
+
 function thePixiJS() {
     let appEl = document.getElementById('the-app');
-    let pixiApp = new PIXI.Application({width: 256, height: 256});
+    let size = findPixiSize();
+    let pixiApp = new PIXI.Application({width: size, height: size, transparent: false});
     appEl.appendChild(pixiApp.view);
+
+    pixiApp.renderer.autoResize = true;
+
+    window.addEventListener('resize', () => {
+        let size = findPixiSize();
+        pixiApp.renderer.resize(size, size);
+    });
 }
 
 theApp();
--- webpack.config.js
+++ webpack.config.js
@@ -10,15 +10,44 @@ module.exports = {
         filename: 'js/[name].js',
         path: path.resolve(__dirname, 'web')
     },
-    devtool: 'inline-source-map',
-    devServer: {
-        contentBase: './web'
-    },
-    plugins: [
+    devServer: { contentBase: './web' }
+};
+
+let theConfig = process.env.THE_CONFIG ? JSON.parse(process.env.THE_CONFIG) : { dev: false, chrome: false};
+
+if (theConfig.dev) {
+    module.exports.devtool = 'inline-source-map';
+    module.exports.plugins = [
         new CleanWebpackPlugin(['web']),
         new HtmlWebpackPlugin({
             title: 'Pixi JS WK',
             minify: false
         })
-    ]
-};
+    ];
+}
+else {
+    module.exports.plugins = [
+        new CleanWebpackPlugin(['web']),
+        new HtmlWebpackPlugin({
+            title: 'Pixi JS WK',
+            minify: true
+        })
+    ];
+}
+
+if (!theConfig.chrome) {
+    module.exports.module = {
+        rules: [
+            {
+                test: /\.es6$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['env'],
+                    }
+                }
+            },
+        ]
+    };
+}
